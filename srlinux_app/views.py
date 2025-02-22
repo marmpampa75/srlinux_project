cbdb0253f8b54260dffba22cfcf9b13d1fa4cf99
root <root@DESKTOP-CS236Q3.>
2025-02-22 15:02:49 +0200

    when login deploy containerlab and when logout destroy

--- srlinux_app/views.py
+++ srlinux_app/views.py
@@ -5,6 +5,17 @@ from .forms import DeviceForm
 from .netmiko_handler import get_device_interfaces
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
+import os
+import subprocess
+
+def run_containerlab():
+    try:
+        print(f"cd clab-quickstart/")  # Debugging
+        os.chdir('/root/clab-quickstart')  # Change directory
+        print(f"sudo containerlab deploy")  # Debugging
+        subprocess.run(['sudo', 'containerlab', 'deploy'], check=True)
+    except Exception as e:
+        print(f"Error running containerlab: {e}")
 
 def device_list(request):
     devices = Device.objects.all()
@@ -34,6 +45,8 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            # Run containerlab after successful login
+            run_containerlab()
             return redirect('device_list')  # Redirect to the device list after login
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials'})
@@ -44,6 +57,15 @@ def logout_confirmation(request):
     return render(request, 'logout_confirmation.html')
 
 def logout_confirmed(request):
+    try:
+        print(f"cd clab-quickstart/")  # Debugging
+        os.chdir('/root/clab-quickstart')  # Change directory
+        print(f"sudo containerlab destroy")  # Debugging
+        subprocess.run(["sudo", "containerlab", "destroy"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error destroying container lab: {e}")
+
+    # Log out the user
     logout(request)
     return redirect('login')  # Redirect the user to the login page after logging out
 
